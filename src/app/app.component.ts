b8bd4e7d69af3f77f315cde4c60d0a66164b213f
W.Bielecki <bva@sibdigital.net>
2022-03-14 02:37:57 +0800

    remove bootstrap and add tailwind. #webcam class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebCam } from './lib/webcameasy';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'webcameasy';
+export class AppComponent implements OnInit{
+
+  webcamWorking:Boolean | undefined
+  myVar1 = false
+  webcam:WebCam | undefined
+  webcamElement:HTMLVideoElement | null = null
+  constructor(){
+    
+  }
+  
+  ngOnInit(): void {
+    this.webcamWorking = false
+    this.webcamElement = <HTMLVideoElement>document.getElementById("webcam")
+    console.log(this.webcamElement)
+    this.webcam = new WebCam(this.webcamElement)
+  }
+  
+  changeSw(){
+    this.webcamWorking = false
+    console.log(this.webcamWorking)
+  }
+
+  startCamera(event:any){
+    const value =  event.target.checked
+    if(value){
+      this.webcam?.start()
+    }else{
+      this.webcam?.stop()
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
